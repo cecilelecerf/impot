11517f86d9941e07ef1b4a4ba1b321570b5ee43b
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-12 12:11:46 +0200

    fix constructor

--- src/class/impot.php
+++ src/class/impot.php
@@ -1,19 +1,19 @@
 <?php 
 
 class Impot {
-    private $income;
+    public $income;
 
     const IMPOT15 = 15;
     const IMPOT20 = 20;
 
-    public static function __constructor(int $income){
+    public function __construct(int $income){
         $this->income = $income;
     }
 
-    private function CalculImpot(){
+    public function CalculImpot(){
         $taux = 0;
         $this->income < 15000 ? $taux = self::IMPOT15 :  $taux = self::IMPOT20;
-        return $this->income * ($taux/100);
+        return $this->income;
     }
 
     public function AfficheImpot(string $prenom){

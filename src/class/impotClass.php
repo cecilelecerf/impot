4ff2782af280d39b4d85d07c0596b724c7a26c36
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-12 12:24:55 +0200

    rename impotClass

---
+++ src/class/impotClass.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Impot {
+    private $income;
+
+    const impot15 = 15;
+    const impot20 = 20;
+
+    public static function __constructor(int $income){
+        $this->income = $income;
+    }
+
+    private function CalculImpot(){
+        $taux = 0;
+        $this->income < 15000 ? $taux = $this->impot15 :  $taux = $this->impot20;
+        return $this->income * ($taux/100);
+    }
+
+    protected function AfficheImpot(string $prenom){
+        return $prenom. "votre impot est de ". $this->calculImpot();
+    }
+}
+?>

5095e37eb4bc9f7f439a98085d94b064aa853fa5
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-12 14:44:21 +0200

    modifie new instance of impot and the call of AfficheImpot

--- src/resultatImpot.php
+++ src/resultatImpot.php
@@ -7,10 +7,10 @@
 </head>
 <?php 
 require_once("./class/impotClass.php");
-$impot = new Impot((int)$_GET["revenu"]);
+$impot = new Impot((int)$_GET["revenu"], $_GET["lastname"], $_GET["firstname"], (int)$_GET["gender"]);
 ?>
 <body>
     <h1>Resultat</h1>
-    <p><?php echo $impot->AfficheImpot($_GET["name"]) ?></p>
+    <p><?php echo $impot->AfficheImpot() ?></p>
 </body>
 </html>

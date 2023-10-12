ec88dfb4cdc1e35de6e6faee6c7dd32c150f506b
Cécile Lecerf <cecel@Cecoule-Macbook.local>
2023-10-12 14:40:42 +0200

    fix constructor + const and method afficheImpot()

--- src/class/impotClass.php
+++ src/class/impotClass.php
@@ -5,21 +5,22 @@ require("./class/peopleClass.php");
 class Impot extends People {
     private $income;
 
-    const impot15 = 15;
-    const impot20 = 20;
+    const TAUX_FAIBLE = 0.15;
+    const TAUX_FORT = 0.20;
 
-    public static function __constructor(int $income){
-        parent::__constructor();
+    public function __construct(int $income, string $lastname, string $firstname, int $gender){
+        parent::__construct($lastname, $firstname, $gender);
         $this->income = $income;
     }
 
     private function CalculImpot(){
         $taux = 0;
-        $this->income < 15000 ? $taux = $this->impot15 :  $taux = $this->impot20;
-        return $this->income * ($taux/100);
+        $this->income < 15000 ? $taux = self::TAUX_FAIBLE :  $taux = self::TAUX_FORT;
+        return $this->income * $taux;
     }
 
-    protected function AfficheImpot(string $prenom){
-        return $prenom. "votre impot est de ". $this->calculImpot();
+    public function AfficheImpot(){
+        $toString = $this->lastname ." ". $this->firstname . " votre impot est de ". $this->CalculImpot(). " €";
+        return $this->gender==0? "Monsieur ". $toString : "Madame ". $toString; 
     }
 }
